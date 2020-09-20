a5954f96b058bed0d6833dbe6011b77a0843b992
Nasser <nasser@kessas.com>
2020-09-20 10:15:55 +1000

    Switched coordinates system

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const MOVEX = 2;
 const CANVAS = { x: 800, y: 450 };
-const GRAVITY = .2;
+const GRAVITY = -.2;
 const MAXSPEED = 10;
 const JUMPHEIGHT = 3.5;
 const KEYDOWN = {}
@@ -17,13 +17,15 @@ const LEFT = 0;
 const RIGHT = 1;
 const COLORS = ["black", "green", "yellow", "orange", "orangered", "red", "blue", "purple"];
 const LEVEL = [
+    // { x: 0, y: 0, width: CANVAS.x/3, height: 25, color: "brown" }, //ground
+
     { x: 0, y: 0, width: CANVAS.x, height: 25, color: "brown" }, //grass
     { x: 0, y: 25, width: CANVAS.x, height: 7, color: "#60ff30" }, //ground
     { x: 550, y: 7 / 10 * CANVAS.y, width: 100, height: 25, color: "brown" }, //platform
     { x: 300, y: 5 / 10 * CANVAS.y, width: 75, height: 25, color: "brown" }, //platform
     { x: 650, y: 4 / 10 * CANVAS.y, width: 150, height: 25, color: "brown" }, //platform
     { x: 00, y: 5 / 10 * CANVAS.y, width: 100, height: 25, color: "brown" }, //platform
-    { x: 100, y: 2 / 10 * CANVAS.y, width: 100, height: 25, color: "brown" }, //platform
+    { x: 100, y: 90, width: 100, height: 25, color: "brown" }, //platform
     { x: 80, y: 3 / 10 * CANVAS.y + 25, width: 50, height: 20, color: "brown" }, //platform
 
     { x: 700, y: 4 / 10 * CANVAS.y + 25, width: 20, height: 20, color: "gold", type: "goal" }  //goal
@@ -50,9 +52,9 @@ class Thing {
         this.Width = width;
         this.Height = height;
         this.Left = x;
-        this.Top = y;
-        this.Bottom = y + height;
-        this.Right = x + width; 
+        this.Top = y + height;
+        this.Bottom = y;
+        this.Right = x + width;
         this.fill = fill;
     }
 
@@ -93,25 +95,25 @@ class Player extends MovingThing {
         super();
         this.Width = 20;
         this.Height = 20;
-        this.Top = y;
+        this.Top = y + this.Height;
         this.Left = x;
-        this.Bottom = y + this.Height;
+        this.Bottom = y;
         this.Right = x + this.Width;
-        this.Bounds = { top: 0, right: CANVAS.x, bottom: CANVAS.y, left: 0 };
+        this.Bounds = { top: CANVAS.y, right: CANVAS.x, bottom: 0, left: 0 };
         this.Fill = COLORS[this.Frame];
         this.getBounds();
     }
 
     getBounds = () => {
-        let b = { top: 0, right: CANVAS.x, bottom: CANVAS.y, left: 0 };
+        let b = { top: CANVAS.y, right: CANVAS.x, bottom: 0, left: 0 };
 
         for (let o of objects) {
             let s = o.getState();
             if (touching(this.Left, this.Right, s.left, s.right)) {
-                if (this.Bottom <= s.top && s.top < b.bottom) b.bottom = s.top;
-                if (this.Top > s.bottom && s.bottom >= b.top) b.top = s.bottom;
+                if (this.Bottom >= s.top && s.top > b.bottom) b.bottom = s.top;
+                if (this.Top < s.bottom && s.bottom <= b.top) b.top = s.bottom;
             }
-            if (touching(this.Top, this.Bottom, s.top, s.bottom)) {
+            if (touching(this.Bottom, this.Top, s.bottom, s.top)) {
                 if (this.Right <= s.left && s.left < b.right) b.right = s.left;
                 if (this.Left >= s.right && s.right > b.left) b.left = s.right;
             }
@@ -122,59 +124,55 @@ class Player extends MovingThing {
 
     move = (direction) => {
         let x = 0;
-        let y = 0;
 
         switch (direction) {
-            case LEFT: x = -MOVEX * this.SpeedX; y = 0; break;
-            case RIGHT: x = MOVEX * this.SpeedX; y = 0; break;
+            case LEFT: x = -MOVEX * this.SpeedX; break;
+            case RIGHT: x = MOVEX * this.SpeedX; break;
         }
 
         this.getBounds()
-
         if (this.Right + x > this.Bounds.right) x = this.Bounds.right - this.Right;
         if (this.Left + x < this.Bounds.left) x = this.Bounds.left - this.Left;
 
         if (!this.jumping()) {
             this.Frame = (this.Frame++ % (COLORS.length - 2)) + 1;
         }
-        this.coords(this.Left + x, this.Top + y);
+        this.coords(this.Left + x, this.Top);
     }
 
-    coords = (x,y) => { this.Left = x; this.Top = y; this.Right = x + this.Width; this.Bottom = y + this.Height; }
+    coords = (x, y) => { this.Left = x; this.Top = y; this.Right = x + this.Width; this.Bottom = y - this.Height; }
 
-    jumping = () => this.Bottom < this.Bounds.bottom;
+    jumping = () => this.Bottom > this.Bounds.bottom;
 
     jump = () => {
 
         if (this.JumpBoost > 0) {
-            this.JumpBoost -= GRAVITY;
+            this.JumpBoost += GRAVITY;
             this.SpeedY -= GRAVITY;
         }
 
         if (this.Bottom != this.Bounds.bottom) return;
         this.JumpBoost = JUMPHEIGHT * 2;
-        this.SpeedY = -JUMPHEIGHT;
-        this.coords(this.Left, this.Top-1)
+        this.SpeedY = JUMPHEIGHT;
+        this.coords(this.Left, this.Top + 1)
     }
 
     update = () => {
+
         if (this.jumping()) {
 
             this.Frame = COLORS.length - 1;
 
             if (this.SpeedY < MAXSPEED) this.SpeedY += GRAVITY;
 
-            if (this.Top + this.SpeedY < this.Bounds.top) {
-                this.Top = this.Bounds.top;
-                this.Bottom = this.Top + this.Height;
+            if (this.Top + this.SpeedY > this.Bounds.top) {
+                this.coords(this.Left, this.Bounds.top);
                 this.SpeedY = 0;
             }
-            else {
-                this.coords(this.Left, this.Top + this.SpeedY);
-            }
+            this.coords(this.Left, this.Top + this.SpeedY);
 
-            if (this.Bottom + this.SpeedY >= this.Bounds.bottom) {
-                this.coords(this.Left, this.Bounds.bottom - this.Height);
+            if (this.Bottom + this.SpeedY <= this.Bounds.bottom) {
+                this.coords(this.Left, this.Bounds.bottom + this.Height);
                 this.SpeedY = 0;
                 this.Frame = 0;
             }
@@ -197,7 +195,7 @@ const redraw = () => {
     for (let o of [player, ...objects]) {
         ctx.beginPath();
         let l = o.getState();
-        ctx.rect(l.left, l.top, l.width, l.height);
+        ctx.rect(l.left, CANVAS.y - l.top, l.width, l.height);
         ctx.fillStyle = l.fill;
         ctx.fill();
     }
