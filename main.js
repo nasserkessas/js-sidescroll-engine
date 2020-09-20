739d6d335484bc0afd5681b29507ee21edcb4d01
Nasser <nasser@kessas.com>
2020-09-20 21:42:38 +1000

    Fixed corner issue

--- main.js
+++ main.js
@@ -237,7 +237,7 @@ class Player extends MovingThing {
 }
 
 const touching = (x1, x2, x3, x4) => {
-    if (x1 > x3 && x1 <= x4) return true;
+    if (x1 > x3 && x1 < x4) return true;
     if (x2 > x3 && x2 <= x4) return true;
     return false;
 }
